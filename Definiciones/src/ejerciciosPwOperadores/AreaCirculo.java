76416ccdaad332249203a9756d807dd38fb556e1
Andrea <ansasa2122daw2@protonmail.com>
2023-01-18 10:58:24 +0100

    Segundo Commit ejercicio Circulo

--- Definiciones/src/ejerciciosPwOperadores/AreaCirculo.java
+++ Definiciones/src/ejerciciosPwOperadores/AreaCirculo.java
@@ -5,7 +5,16 @@ public class AreaCirculo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int radio;
+		double areaCirculo;
+		double longitud;
+		
+		radio=23;
+		areaCirculo = Math.PI * Math.pow(radio, 2); //otra forma (radio * radio)
+		longitud = 2 * Math.PI * radio;
+		
+		System.out.println("\nRadio: "+ radio + "\nArea Circulo: " + areaCirculo + "\nLongitud: " + longitud);
+		
 
 	}
 
