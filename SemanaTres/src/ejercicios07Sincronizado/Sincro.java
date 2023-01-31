1b911e5f57842083860b2778b4fc8c61cb7230da
Andrea <ansasa2122daw2@protonmail.com>
2023-01-31 11:31:20 +0100

    Commit ejemplo Sincronizado (synchronized)

---
+++ SemanaTres/src/ejercicios07Sincronizado/Sincro.java
@@ -0,0 +1,25 @@
+package ejercicios07Sincronizado;
+
+public class Sincro implements Runnable{
+
+	static Integer n = 1;
+	private String mensaje;
+	
+	public void setMensaje(String mensaje) {
+		this.mensaje = mensaje;
+	}
+	
+	public void run() {
+		for (int i = 0; i < 10; i++) {
+			synchronized(n) {
+				System.out.println(mensaje + " " + n);
+				n++;
+			}
+			try {
+				Thread.sleep(500);
+			}catch(InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
