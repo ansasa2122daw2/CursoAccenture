36586d40974bfff5557617f240de1d7f9eac8e44
Andrea <ansasa2122daw2@protonmail.com>
2023-02-03 14:59:08 +0100

    Commit JDBC

---
+++ SemanaTres/src/ejercicios12BBDD0/Country.java
@@ -0,0 +1,30 @@
+package ejercicios12BBDD0;
+
+public class Country {
+	
+	private String country_id;
+	private String cuntry_name;
+	private String region_id;
+	
+	public String getCountry_id() {
+		return country_id;
+	}
+	public void setCountry_id(String country_id) {
+		this.country_id = country_id;
+	}
+	public String getCuntry_name() {
+		return cuntry_name;
+	}
+	public void setCuntry_name(String cuntry_name) {
+		this.cuntry_name = cuntry_name;
+	}
+	public String getRegion_id() {
+		return region_id;
+	}
+	public void setRegion_id(String region_id) {
+		this.region_id = region_id;
+	}
+	
+	
+
+}
